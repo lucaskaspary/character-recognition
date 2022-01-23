1802977b4e706db0819be5082df72d8e1f27a143
lucaskaspary <lucaskaspary@hotmail.com>
2022-01-23 09:22:19 -0300

    Initial commit

---
+++ get_and_build_opencv.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+echo "OpenCV installation by learnOpenCV.com"
+
+# Define OpenCV version to install
+cvVersion="master"
+
+# Clean build directories
+rm -rf opencv/build
+rm -rf opencv_contrib/build
+
+# Create directory for installation
+mkdir installation
+mkdir installation/OpenCV-"$cvVersion"
+
+# Save current working directory
+cwd=$(pwd)
+
+cd $cwd
+############ For Python 3 ############
+# create virtual environment
+python3 -m venv OpenCV-"$cvVersion"-py3
+echo "# Virtual Environment Wrapper" >> ~/.bashrc
+echo "alias workoncv-$cvVersion=\"source $cwd/OpenCV-$cvVersion-py3/bin/activate\"" >> ~/.bashrc
+source "$cwd"/OpenCV-"$cvVersion"-py3/bin/activate
+
+# now install python libraries within this virtual environment
+pip install wheel numpy scipy matplotlib scikit-image scikit-learn ipython dlib
+
+# quit virtual environment
+deactivate
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout $cvVersion
+cd ..
+
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout $cvVersion
+cd ..
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+            -D CMAKE_INSTALL_PREFIX=$cwd/installation/OpenCV-"$cvVersion" \
+            -D INSTALL_C_EXAMPLES=ON \
+            -D INSTALL_PYTHON_EXAMPLES=ON \
+            -D WITH_TBB=ON \
+            -D WITH_V4L=ON \
+            -D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.5/site-packages \
+        -D WITH_QT=ON \
+        -D WITH_OPENGL=ON \
+        -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+        -D BUILD_EXAMPLES=ON ..
+
+make -j4
+make install
+
+
+
+
+
+
+
